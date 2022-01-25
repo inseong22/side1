22c21d2a29e5be066e99e641f606b6ddcabd1d15
yexjin <tnska0421@naver.com>
2022-01-25 19:56:00 +0900

    error handling

---
+++ node_modules/rc-field-form/es/index.d.ts
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { FormInstance } from './interface';
+import Field from './Field';
+import List from './List';
+import useForm from './useForm';
+import { FormProps } from './Form';
+import { FormProvider } from './FormContext';
+import FieldContext from './FieldContext';
+import ListContext from './ListContext';
+declare const InternalForm: <Values = any>(props: FormProps<Values> & {
+    children?: React.ReactNode;
+} & {
+    ref?: React.Ref<FormInstance<Values>>;
+}) => React.ReactElement;
+declare type InternalForm = typeof InternalForm;
+interface RefForm extends InternalForm {
+    FormProvider: typeof FormProvider;
+    Field: typeof Field;
+    List: typeof List;
+    useForm: typeof useForm;
+}
+declare const RefForm: RefForm;
+export { FormInstance, Field, List, useForm, FormProvider, FormProps, FieldContext, ListContext };
+export default RefForm;
