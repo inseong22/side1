af1b08e8b12f800e2f00a647838bcd9fa48e9442
yexjin <tnska0421@naver.com>
2022-02-14 10:01:29 +0900

    Layout text section

--- src/components/Make/SectionTypes/QnaSection.js
+++ src/components/Make/SectionTypes/QnaSection.js
@@ -15,11 +15,10 @@ function QnaSection({content, setting}) {
             <QnaOpenClose key={index} title={item.question} open={content.qna.shape === 'open'} color={content.qna.question} content={content} type={content.layout} index={index}>
                 <div className="edit-element">
                     <div style={{display:'flex', alignItems: 'start', height:'100%'}}>
-                        <span className="qna__word">A. <></></span>
+                        <span className="qna__word" style={{fontFamily:`${state.setting.smallFont}`}}>A. <></></span>
                     </div>
                     <TextAuto 
                         small
-                        style={{fontFamily:`${state.setting.smallFont}`}}
                         value={item.answer} 
                         onChange={e => action.setContents(produce(state.contents, draft => {
                             draft[state.secNum].qnas[index].answer = e.currentTarget.value;
@@ -35,11 +34,10 @@ function QnaSection({content, setting}) {
             data-aos={setting.animation} aos-duration="4000">
 
             <TitleDesc content={content} />
-            {content.qna.use && 
-                <div className="features__container" style={{flexDirection: 'column', marginTop:'20px'}}>
-                    {returnQnaCards}
-                </div>
-            }
+
+            <div className="features__container" style={{flexDirection: 'column', marginTop:'20px'}}>
+                {returnQnaCards}
+            </div>
 
         </motion.div>
     )
--- src/components/Make/SectionTypes/TextSection.js
+++ src/components/Make/SectionTypes/TextSection.js
@@ -6,10 +6,38 @@ import TitleDesc from './components/TitleDesc'
 
 function TextSection({content, setting}) {
 
+    const {state, action} = useContext(MyContext) //ContextAPI로 state와 action을 넘겨받는다.
+    if (content.layout === 1)
+        {
+            action.setContents(produce(state.contents, draft => {
+                draft[state.secNum].title.align='center'
+                draft[state.secNum].desc.align='center'
+                draft[state.secNum].padding.top= 15
+                draft[state.secNum].padding.bottom= 15
+            }))
+        }
+    else if(content.layout === 2)
+    {
+        action.setContents(produce(state.contents, draft => {
+            draft[state.secNum].title.align='left'
+            draft[state.secNum].desc.align='left'
+            draft[state.secNum].padding.top= 25
+            draft[state.secNum].padding.bottom= 5
+        }))
+    }
+    else
+    {
+        action.setContents(produce(state.contents, draft => {
+            draft[state.secNum].title.align='left'
+            draft[state.secNum].desc.align='left'
+            draft[state.secNum].padding.top= 15
+            draft[state.secNum].padding.bottom= 15
+        }))
+    }
+
     return (
         <motion.div className="centera"
             data-aos={setting.animation} aos-duration="4000">
-
             <TitleDesc content={content} />
 
         </motion.div>
