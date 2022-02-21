c77c4565f0964e75802850e6de09c192979a2c01
Hojin Kim <zxcv05999@naver.com>
2022-02-21 22:18:19 +0900

    Mix

--- src/components/Make/SectionTypes/components/ReturnButton.js
+++ src/components/Make/SectionTypes/components/ReturnButton.js
@@ -49,7 +49,6 @@ function ReturnButton({content, onlyapp}){
             </div>
         )
     }
-<<<<<<< HEAD
 
     return (
         <>
@@ -66,7 +65,9 @@ function ReturnButton({content, onlyapp}){
                         <img src={appstorebutton} className="store-button" />
                 }
             </div>) 
-        : (content.button.use && 
+        : 
+        <>
+        {content.button.use && 
                     <div style={{width:'100%'}}>
                         <div className="button__container" style={{
                             justifyContent:`${state.isPhone ? content.mobile.align : content.button.align}`,
@@ -93,65 +94,23 @@ function ReturnButton({content, onlyapp}){
                             }
                             </>
                         </div>
-                        {
-                            content.appButton.use && 
-                            <div className="button__container" style={{justifyContent:`${state.isPhone ? content.mobile.align : content.appButton.align}`}}>
-                                {
-                                    content.appButton.google.length > 0 && 
-                                        <img src={playstorebutton} className="store-button" />
-                                }
-                                {/* onClick={() => {window.open(`${content.button.ghostLink}`)}} */}
-                                {
-                                    content.appButton.apple.length > 0 && 
-                                        <img src={appstorebutton} className="store-button" />
-                                }
-                            </div>
-                        }
                     </div>
-        )}
-=======
-    {/* <CustomCtaButton className="action-button" onClick={() => {window.open(`${content.button.ctaLink}`)}}> */}
-    return(
-        <>
-            {
-                content.button.use && 
-                <div className="button__container" style={{
-                    justifyContent:`${state.isPhone ? content.mobile.align : content.button.align}`,
-                    flexDirection:`${ 
-                        content.button.ctaUse && content.button.ctaOption === 'apply' && 
-                        content.button.ghostUse && content.button.ghostOption === 'apply' ? 'column' : 'row'
-                        }`
-                    }}>
-                    <>
-                    {
-                        content.button.ctaUse && 
-                            ( content.button.ctaOption === 'link' ? CustomButton('cta') : returnInputs('cta') )
-                    }
-                    </>
-                    <>
-                    {/* onClick={() => {window.open(`${content.button.ghostLink}`)}} */}
-                    {
-                        content.button.ghostUse && 
-                            ( content.button.ghostOption === 'link' ? CustomButton('ghost') : returnInputs('ghost') )
-                    }
-                    </>
-                </div>
-            }
-            {
-                content.appButton.use && 
-                <div className="button__container" style={{justifyContent:`${state.isPhone ? content.mobile.align : content.appButton.align}`}}>
-                    {
-                        content.appButton.google.length > 0 && 
-                            <img src={playstorebutton} className="store-button" style={{height:'51px'}} />
-                    }
-                    {/* onClick={() => {window.open(`${content.button.ghostLink}`)}} */}
-                    {
-                        content.appButton.apple.length > 0 && 
-                            <img src={appstorebutton} className="store-button" />
-                    }
-                </div>
-            }
->>>>>>> 1b756eb41 (Response Tutorail)
+        }
+        {
+            content.appButton.use && 
+            <div className="button__container" style={{justifyContent:`${state.isPhone ? content.mobile.align : content.appButton.align}`}}>
+                {
+                    content.appButton.google.length > 0 && 
+                        <img src={playstorebutton} className="store-button" style={{height:`${state.isPhone ? '31px' : '51px'}`}} />
+                }
+                {/* onClick={() => {window.open(`${content.button.ghostLink}`)}} */}
+                {
+                    content.appButton.apple.length > 0 && 
+                        <img src={appstorebutton} className="store-button" style={{height:`${state.isPhone ? '30px' : '50px'}`}} />
+                }
+            </div>
+        }
+        </>}
         </>
     )
 }
